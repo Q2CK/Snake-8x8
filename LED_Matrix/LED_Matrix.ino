f2c8b8d1703e3e5d71367f533db2e86caecd3293
Q2CK <109765887+Q2CK@users.noreply.github.com>
2022-09-16 18:52:34 +0200

    Add files via upload

--- LED_Matrix/LED_Matrix.ino
+++ LED_Matrix/LED_Matrix.ino
@@ -5,101 +5,124 @@ byte CS = 7;
 byte CLK = 13;
 LedControl lc=LedControl(DIN, CLK, CS,0);
 
-byte snake_x[64] = {};
-byte snake_y[64] = {};
+byte snake_x[64];
+byte snake_y[64];
 
 byte snake_length;
 
 byte dir_x;
 byte dir_y;
 
+byte dir_choice;
+
 byte food_x;
 byte food_y;
 
 bool loss;
 
+bool push_l;
+bool push_r;
+
 void initialize() {
   for(byte i = 0; i < 8; i++) {
     lc.setRow(0, i, 0);
   }
-  
-  randomSeed(analogRead(A0));
 
   lc.shutdown(0,false);
   lc.setIntensity(0,7);
   
-  food_x = random(0, 8);
-  food_y = random(0, 8);
+  food_x = analogRead(A0) % 8;
+  food_y = analogRead(A0) % 8;
 
   dir_x = 0;
   dir_y = 1;
 
+  dir_choice = 1;
+  push_l = false;
+  push_r = false;
+
   loss = false;
 
   snake_length = 4;
-  
-  snake_x[0] = 3;
-  snake_x[1] = 3;
-  snake_x[2] = 3;
-  snake_x[3] = 3;
-  snake_x[4] = 4;
-  
-  snake_y[0] = 0;
-  snake_y[1] = 1;
-  snake_y[2] = 2;
-  snake_y[3] = 3;
-  snake_y[4] = 4;
+
+  for(byte i = 0; i < 5; i++) {
+    snake_x[i] = 3;
+    snake_y[i] = i;
+  }
 }
 
 void setup() {
   
   initialize();
+
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
   
-  Serial.begin(9600);
 }
   
 void loop() {
   if(!loss) {
-  
-    if(Serial.available()) {   
-      switch(Serial.read()) {     
-        case 'w':
-          dir_x = 0;
-          dir_y = 1;
-          break;
-          
-        case 'a':
-          dir_x = 1;
-          dir_y = 0;
-          break;
-          
-        case 's':
-          dir_x = 0;
-          dir_y = -1;
-          break;
-          
-        case 'd':
-          dir_x = -1;
-          dir_y = 0;
-          break;
-      }
+    if(push_l == false and digitalRead(8) == LOW) {
+      push_l = true;
+      dir_choice--;
     }
     
-    if(millis() % 200 == 0) {
+    if(push_r == false and digitalRead(9) == LOW) {
+      push_r = true;
+      dir_choice++;
+    }
+    
+    if(millis() % 150 < 2) {
+      if(dir_choice < 1)
+      dir_choice = 4;
+      
+      else if(dir_choice > 4)
+        dir_choice = 1;
+  
+      switch(dir_choice) {
+        
+        case 1:
+          dir_y = 1;
+          dir_x = 0;
+          break;
+          
+        case 2:
+          dir_y = 0;
+          dir_x = 1;
+          break;
+  
+        case 3:
+          dir_y = -1;
+          dir_x = 0;
+          break;
+  
+        case 4:
+          dir_y = 0;
+          dir_x = -1;
+          break;
+      }
+
+      if(push_r == true and digitalRead(9) == HIGH) {
+        push_r = false;
+      }
+
+      if(push_l == true and digitalRead(8) == HIGH) {
+        push_l = false;
+      }
       
       lc.setLed(0, snake_y[0], snake_x[0], false);
       
       lc.setLed(0, food_y, food_x, true);
       
-      if(snake_x[snake_length - 1] % 8 == food_x && snake_y[snake_length - 1] % 8 == food_y) {
+      if(snake_x[snake_length - 1] == food_x && snake_y[snake_length - 1] == food_y) {
         
         snake_length++;
         
         snake_x[snake_length - 1] = food_x;
         snake_y[snake_length - 1] = food_y;
   
-        food_x = random(0, 8);
-        food_y = random(0, 8);
+        food_x = analogRead(A0) % 8;
+        food_y = analogRead(A0) % 8;
         
       }    
       
@@ -123,7 +146,7 @@ void loop() {
       for(byte i = 0; i < snake_length - 2; i++) {
         
         if(snake_x[i] == snake_x[snake_length - 1] && snake_y[i] == snake_y[snake_length - 1]) {
-          
+           
           loss = true;
           
         }       
@@ -131,7 +154,6 @@ void loop() {
     }
   }
   else {
-    delay(2000);
     initialize();
   }
 }
